84694b4ad515634e2d6f77aee9eeca4a8033f11f
Stefani Hatton <stefani.hatton@gmail.com>
2018-11-28 20:51:51 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,3 +1,9 @@
 function shout(string) {
   return string.toUpperCase()
 }  
+function whisper(string) {
+  return 'hello'.toLowerCase()
+}
+function logShout(string) {
+  console.log; 'hello'.UpperCase()
+}
